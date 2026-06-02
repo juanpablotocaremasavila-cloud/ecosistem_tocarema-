7986ef654b92c7dcda1898022dfd44f2b2080c93
juan pablo tocarema avila <juanpablotocaremaavila@gmail.com>
2026-06-02 08:05:38 -0500

    chore: update clever cloud db credentials

--- node/database/db.js
+++ node/database/db.js
@@ -27,14 +27,14 @@ if (!loaded) {
 }
 
 const db = new Sequelize(
-    process.env.DB_NAME || process.env.MYSQL_ADDON_DB || 'bszqtbaswf3bikfeiph4',
-    process.env.DB_USER || process.env.MYSQL_ADDON_USER || 'ueub8gpermio6wet',
-    process.env.DB_PASSWORD || process.env.MYSQL_ADDON_PASSWORD || 'iT8LN2JuVkWdUgrNDy0T', 
+    process.env.DB_NAME || process.env.MYSQL_ADDON_DB || 'b0py2ume77nsk8dp10gz',
+    process.env.DB_USER || process.env.MYSQL_ADDON_USER || 'utjsesrlthfz0w06',
+    process.env.DB_PASSWORD || process.env.MYSQL_ADDON_PASSWORD || '5MPAVxyGFrU29AKQAkWw', 
     {
-        host: process.env.DB_HOST || process.env.MYSQL_ADDON_HOST || 'bszqtbaswf3bikfeiph4-mysql.services.clever-cloud.com',
+        host: process.env.DB_HOST || process.env.MYSQL_ADDON_HOST || 'b0py2ume77nsk8dp10gz-mysql.services.clever-cloud.com',
         port: process.env.DB_PORT || process.env.MYSQL_ADDON_PORT || 3306,
         dialect: 'mysql',
-        dialectOptions: (process.env.DB_HOST && process.env.DB_HOST !== 'localhost') || process.env.MYSQL_ADDON_HOST || 'bszqtbaswf3bikfeiph4' ? {
+        dialectOptions: (process.env.DB_HOST && process.env.DB_HOST !== 'localhost') || process.env.MYSQL_ADDON_HOST || 'b0py2ume77nsk8dp10gz' ? {
             ssl: {
                 require: true,
                 rejectUnauthorized: false
